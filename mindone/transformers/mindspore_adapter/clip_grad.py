e44b43cd848955c024c26900eee732c2735d305a
zhanghuiyao <1814619459@qq.com>
2024-11-13 11:04:50 +0800

    debug

--- mindone/transformers/mindspore_adapter/clip_grad.py
+++ mindone/transformers/mindspore_adapter/clip_grad.py
@@ -86,7 +86,7 @@ def _clip_grad_l2norm_for_zero(max_norm, all_reduce_op, part_grads):
     grads_square_sum = hyper_map_op(F.partial(_square_sum_and_all_reduce, all_reduce_op), part_grads)
     total_norm = ops.sqrt(ops.addn(grads_square_sum))
 
-    ops.TensorDump()("total_norm", total_norm.astype(ms.float32))
+    # ops.TensorDump()("total_norm", total_norm.astype(ms.float32))
 
     clip_coef = max_norm / (total_norm + 1e-6)
     clip_coef_clamped = ops.clamp(clip_coef, None, 1.0)
