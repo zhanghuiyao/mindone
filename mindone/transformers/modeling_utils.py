e223dbb58ea52a68d6801c9d3c11a01d5713574e
zhanghuiyao <1814619459@qq.com>
2025-10-29 20:58:55 +0800

    1

--- mindone/transformers/modeling_utils.py
+++ mindone/transformers/modeling_utils.py
@@ -509,7 +509,8 @@ class ModuleUtilsMixin:
 
     def to(self, dtype: Optional[ms.Type] = None):
         for p in self.get_parameters():
-            p.set_dtype(dtype)
+            if p.is_floating_point():
+                p.set_dtype(dtype)
         return self
 
     def float(self):
